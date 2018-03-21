ecda3f67516e5cde1f1ba3c57e6fcf87561a887a
Chamus Gilbert <chamusg@gmail.com>
2018-03-21 08:34:06 -0400

    Initial commit

---
+++ ProjectEuler.net/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ProjectEuler.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
