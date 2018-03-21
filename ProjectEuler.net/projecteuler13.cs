b117be41fa4fe879cccd16d6d3c3fa0cd27a9bf6
Chamus Gilbert <chamusg@gmail.com>
2018-03-21 09:23:50 -0400

    Added streamreader and ListSum Method

---
+++ ProjectEuler.net/projecteuler13.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace ProjectEuler.net
+{
+    public class projecteuler13
+    {
+        public projecteuler13()
+        {
+            List<double> listOfNumbers = new List<double>();
+            string numberString;
+            StreamReader projectEulerNumber = new StreamReader(@"/Users/FamousChamus/Projects/ProjectEuler.net/ProjectEuler.net/projecteuler13.txt");
+            while (true)
+            {
+                numberString = projectEulerNumber.ReadLine();
+                if (numberString == null)
+                {
+                    break;
+                }
+                listOfNumbers.Add(double.Parse(numberString));
+            }
+            ListSum(listOfNumbers);
+
+        }
+        public double ListSum(List<double> listOfNumbers)
+        {
+            double sum = 0;
+            foreach (var item in listOfNumbers)
+            {
+                sum += item;
+            }
+            return sum;
+        }
+    }
+}
